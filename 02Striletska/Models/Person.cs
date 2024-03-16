61bc6403bd1377c1ca3f7adfc938495f21b15010
Striletska Kateryna <striletskay2005@gmail.com>
2024-03-16 10:20:38 +0200

    Add project files.

---
+++ 02Striletska/Models/Person.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace _02Striletska.Models
+{
+    internal class Person 
+    {
+        #region Fields
+        private string _firstName;
+        private string _lastName;
+        private string _email;
+        private DateTime _birthday;
+        private bool _isAdult;
+        private string _chineseSign; 
+        private string _sunSign;
+        private bool _isBirthday;
+        #endregion
+
+        #region Constructors
+        public Person(string firstName, string lastName, string email, DateTime birthday)
+        {
+            FirstName = firstName;
+            LastName = lastName;    
+            Email = email;
+            Birthday = birthday;
+            _isAdult = canculateIsAdult();
+            _chineseSign = canculateChineseSign();  
+            _sunSign = canculateSunSign();  
+            _isBirthday = isTodayBirthday();
+            Thread.Sleep(1000);
+        }
+        public Person(string name, string lastName, string email):this(name, lastName, email, DateTime.MinValue)  {}
+        public Person(string name, string lastName, DateTime birthday): this (name, lastName, "", birthday) {}
+        #endregion
+
+        #region Properties
+        public string FirstName
+        {
+            get { return _firstName; }
+            set { _firstName = value; }
+           
+        }
+        public string LastName
+        {
+            get { return _lastName; }
+            set { _lastName = value; }
+        }
+        public DateTime Birthday
+        {
+            get { return _birthday; }
+            set { _birthday = value; }  
+        }
+        public string Email
+        {
+            get { return _email; }
+            set {  _email = value; } 
+        }
+        public bool IsAdult
+        {
+            get { return _isAdult; }
+        }
+        public string SunSign
+        {
+            get { return _sunSign; }
+        }
+        public string ChineseSign
+        {
+            get { return _chineseSign; }
+        }
+        public bool IsBirthday
+        {
+            get { return _isBirthday; }
+        }
+        #endregion
+        public bool canculateIsAdult()
+        {
+            int age = DateTime.Now.Year - Birthday.Year;
+            if (DateTime.Now > Birthday)
+                age--;
+            return age > 18;
+        }
+        public string canculateSunSign()
+        {
+            if ((Birthday.Month == 03 && Birthday.Day >= 21) || (Birthday.Month == 04 && Birthday.Day <= 20))
+                return "Aries";
+            if ((Birthday.Month == 04 && Birthday.Day >= 21) || (Birthday.Month == 05 && Birthday.Day <= 21))
+                return "Taurus";
+            if ((Birthday.Month == 05 && Birthday.Day >= 22) || (Birthday.Month == 06 && Birthday.Day <= 21))
+                return "Gemini";
+            if ((Birthday.Month == 06 && Birthday.Day >= 22) || (Birthday.Month == 07 && Birthday.Day <= 22))
+                return "Cancer";
+            if ((Birthday.Month == 07 && Birthday.Day >= 23) || (Birthday.Month == 08 && Birthday.Day <= 23))
+                return "Leo";
+            if ((Birthday.Month == 08 && Birthday.Day >= 24) || (Birthday.Month == 09 && Birthday.Day <= 23))
+                return "Virgo";
+            if ((Birthday.Month == 09 && Birthday.Day >= 24) || (Birthday.Month == 10 && Birthday.Day <= 23))
+                return "Libra";
+            if ((Birthday.Month == 10 && Birthday.Day >= 23) || (Birthday.Month == 11 && Birthday.Day <= 22))
+                return "Scorpion";
+            if ((Birthday.Month == 11 && Birthday.Day >= 23) || (Birthday.Month == 12 && Birthday.Day <= 22))
+                return "Sagittarius";
+            if ((Birthday.Month == 12 && Birthday.Day >= 23) || (Birthday.Month == 01 && Birthday.Day <= 20))
+                return "Capricorn";
+            if ((Birthday.Month == 01 && Birthday.Day >= 21) || (Birthday.Month == 02 && Birthday.Day <= 20))
+                return "Aquarius";
+            return "Pisces";
+        }
+        public string canculateChineseSign()
+        {
+            string[] chineseZodiacSigns = { "Mouse", "Ox", "Tiger", "Rabbit", "Dragon", "Snake", "Horse", "Sheep", "Monkey", "Cock", "Dog", "Pig" };
+            return chineseZodiacSigns[(Birthday.Year - 1888) % 12];
+        }
+        public bool isTodayBirthday()
+        { return DateTime.Now.Day == Birthday.Day && DateTime.Now.Month == Birthday.Month; }
+    }
+}
---
+++ 02Striletska/ViewModels/PersoneViewModel.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Windows.Controls;
+using System.Windows;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using _02Striletska.Models;
+using _02Striletska.Tools;
+using System.Runtime.CompilerServices;
+using System.Diagnostics;
+using System.Xml.Linq;
+
+namespace _02Striletska.ViewModels
+{
+    internal class PersoneViewModel : INotifyPropertyChanged
+    {
+        private Person? _person;
+        private RelayCommand<object> _inputDateCommand;
+
+        private string _firstName;
+        private string _lastName;
+        private string _email;
+        private DateTime _birthday;
+        private bool _isEnabled = true;
+
+        #region Properties
+        public bool IsEnabled
+        {
+            get { return _isEnabled; }
+            set 
+            {
+                _isEnabled = value;
+                OnPropertyChanged(nameof(IsEnabled));
+            }   
+        }
+        public string FirstName
+        {
+            get 
+            {
+                return _person.FirstName;
+            }
+            set
+            {
+               
+             
+                _firstName = value;
+                
+            }
+        }
+        public string LastName
+        {
+            get { return _person.LastName; }
+            set 
+            { 
+                
+         
+                _lastName = value;
+                
+            }
+        }
+        public DateTime Birthday
+        {
+            get { return _person.Birthday; }
+            set 
+            {
+
+                _birthday = value;
+               
+
+            }
+        }
+        public string Email
+        {
+            get { return _person.Email; }
+            set 
+            {
+               
+         
+                _email = value;
+               
+
+            }
+        }
+        public bool IsAdult
+        {
+            get { return _person.IsAdult; }
+        }
+        public string SunSign
+        {
+            get { return _person.SunSign; }
+        }
+        public string ChineseSign
+        {
+            get { return _person.ChineseSign; }
+        }
+        public bool IsBirthday
+        {
+            get { return _person.IsBirthday; }
+        }
+        #endregion
+            
+        public RelayCommand<object> ProceedCommand
+        {
+            get { return _inputDateCommand ??= new RelayCommand<object>(_ => Proceed(), CanProceed); }
+
+
+        }
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        private bool CanProceed(object obj)
+        {
+            return !string.IsNullOrWhiteSpace(_firstName)
+        && !string.IsNullOrWhiteSpace(_lastName)
+        && !string.IsNullOrWhiteSpace(_email)
+        && _birthday != DateTime.MinValue;
+        }
+        private async void Proceed()
+        {
+          
+         
+                try
+                {
+                    IsEnabled = false;
+                    _person = await Task.Run(() => new Person(_firstName, _lastName, _email, _birthday));
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"Exception: {ex.Message}");
+                }
+                finally
+                {
+                int age = DateTime.Now.Year - Birthday.Year;
+                if (DateTime.Now > Birthday)
+                    age--;
+                if (age >= 135 || Birthday > DateTime.Now)
+                {
+                    _person = null;
+                    MessageBox.Show("It's not valid Date!", "Whoops, Exception!", MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+                else
+                {
+
+                   
+                    OnPropertyChanged(nameof(FirstName));
+                    OnPropertyChanged(nameof(LastName));
+                    OnPropertyChanged(nameof(Email));
+                    OnPropertyChanged(nameof(Birthday));
+                    OnPropertyChanged(nameof(IsAdult));
+                    OnPropertyChanged(nameof(IsBirthday));
+                    OnPropertyChanged(nameof(SunSign));
+                    OnPropertyChanged(nameof(ChineseSign));
+                    if (Birthday.Day == DateTime.Now.Day && DateTime.Now.Month == Birthday.Month && Birthday.Year == DateTime.Now.Year)
+                        MessageBox.Show("I congratulate you on today's birthday. Be happy and smile a lot!", "Who have a birthday today!?", MessageBoxButton.OK, MessageBoxImage.Asterisk);
+
+                }
+                IsEnabled = true;
+            }
+            
+        }
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
